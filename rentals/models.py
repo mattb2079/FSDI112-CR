451ea00e5498da4c3794be3794339d684052a00e
mattb2079 <mattb2079@yahoo.com>
2020-04-09 23:16:20 -0700

    first commit

---
+++ rentals/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+# Create your models here.
+
+'''
+field:
+    Char
+    Int
+    Float
+    Boolean
+'''
+
+#Migration steps
+# 1 - Make migrations
+# 2 - (migrate) apply migrations
+
+# Create your models here
+
+class Genre(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length = 255)
+    star = models.CharField(max_length = 255)
+    release_year = models.IntegerField()
+    price = models.FloatField()
+    in_stock = models.BooleanField()
+    # have 1 to many relations with genre
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    in_4k = models.BooleanField()
+    director = models.CharField(max_length = 255)
+    image_url = models.TextField()
+
+    #def __str__(self):
+    #   return str(self.release_year) + self.title
+
+class Country(models.Model):
+    name = models.CharField(max_length = 255)
